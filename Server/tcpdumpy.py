a5591ab7bc936c97b54612d6f8054724af1d0a08
dweitzel <dweitzel@1fda6f16-4416-0410-bfd6-867a04880151>
2010-03-29 19:12:02 +0000

    Added some optimizations for port numbers
    git-svn-id: svn://t2.unl.edu/brian/HadoopViz/branches/SecViz@3858 1fda6f16-4416-0410-bfd6-867a04880151

--- Server/tcpdumpy.py
+++ Server/tcpdumpy.py
@@ -7,22 +7,23 @@ import sys,re,select,time
 
 
 from socket import *
-line_re = re.compile(".*IP (\d+.\d+.\d+.\d+).(\d+).*> (\d+.\d+.\d+.\d+).(\d+)")
+line_re = re.compile(".*IP (\d+.\d+.\d+.\d+).([\d|\w]+)\s+>\s+(\d+.\d+.\d+.\d+).([\d|\w]+)")
 host = "129.93.229.173"
 
 
 def ParseLine(line):
    regex_line = line_re.search(line)
    if regex_line:
+#      print line
       return regex_line.groups()
 
 
 sock = socket(AF_INET, SOCK_DGRAM)
 
 def SendParsed(parsed):
-   if host not in parsed:
-      sock.sendto("packet " + " ".join( ( parsed[0], parsed[2], parsed[1], parsed[3] ) ), (host, 9345))
-      print " ".join( (parsed[0], parsed[2], parsed[1], parsed[3]) )
+   #if host not in parsed:
+   sock.sendto("packet " + " ".join( ( parsed[0], parsed[2], parsed[1], parsed[3] ) ), (host, 9345))
+#   print " ".join( (parsed[0], parsed[2], parsed[1], parsed[3]) )
 
 def ReadOutput(tcpdump_out):
    while 1:
@@ -33,9 +34,9 @@ def ReadOutput(tcpdump_out):
       else:
          time.sleep(.01)
 
-
+# /usr/sbin/tcpdump -t -n -i eth1 ip host not 129.93.229.226
 def main():
-   tcpdump_proc = subprocess.Popen("/usr/sbin/tcpdump -n -i en1", stdout=subprocess.PIPE, shell=True, stderr=subprocess.PIPE)
+   tcpdump_proc = subprocess.Popen("/usr/sbin/tcpdump -tn -i eth1 ip host not %s" % host, stdout=subprocess.PIPE, shell=True, stderr=subprocess.PIPE)
    tcp_stdout = tcpdump_proc.stdout
    tcp_stderr = tcpdump_proc.stderr
    while len(select.select( [tcp_stderr], [], [], 0.0)[0]) != 0:
